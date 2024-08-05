573d15e775736f5eea799955a1f03e309e18b3d2
Vincent Kyalo <kyalovincent@yahoo.com>
2024-08-05 14:36:04 +0200

    more wipping

--- src/table.h
+++ src/table.h
@@ -4,18 +4,19 @@
 #include "common.h"
 #include "value.h"
 
-typedef struct
-{
-    int count;
-    int capacity;
-    Entry *entries;
-} Table;
 
 typedef struct
 {
     ObjString *key;
     Value value;
 } Entry;
+
+typedef struct
+{
+    int count;
+    int capacity;
+    Entry *entries;
+} Table;
 
 void initTable(Table *table);
 void freeTable(Table *table);
